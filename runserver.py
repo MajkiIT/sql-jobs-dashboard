b91ac230d5bde38081171fc710acd482aa90bcfb
Maciej M <michalowski.ma@gmail.com>
2018-12-07 12:31:51 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -8,9 +8,9 @@ from dashboard import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '5556'))
     except ValueError:
-        PORT = 5555
+        PORT = 5556
     app.run(HOST, PORT, threaded=True)
 
     
