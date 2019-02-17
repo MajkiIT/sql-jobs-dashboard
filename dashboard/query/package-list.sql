1f43de786c2ff2c5e243072abba38b550c1b9853
Tomasz <t.cieplinski@gmail.com>
2019-02-18 00:09:11 +0100

    Update package-list.sql

--- dashboard/query/package-list.sql
+++ dashboard/query/package-list.sql
@@ -7,69 +7,117 @@ DECLARE @executionCount INT = ?;
 
 SET @asOfDate = ISNULL(@asOfDate, SYSDATETIME());
 
-with cteWE as
+with cte
+  AS
+  (
+  select ROW_NUMBER() over (Partition by Job_id order by instance_id) rn, job_id,instance_id  
+  from msdb.dbo.sysjobhistory jh
+  where step_id = 0
+  ), cte_jobs
+  as
+  (
+	select
+	instance_id,
+	run_date,
+	run_time,
+	run_duration,
+	run_status,
+	[server],
+	step_name,
+	job_id 
+from msdb.dbo.sysjobhistory
+  ),cte_join
+  AS
+  (
+  select 
+  a.job_id, 
+  b.instance_id lb_instance_id,
+  a.instance_id 
+  from cte a
+  left join cte b
+  on a.rn = b.rn+1 and a.job_id = b.job_id
+  ),
+  cte_executions
+  AS
+  (
+    select  
+	statistics_id = a.instance_id,
+	execution_id = b.instance_id,
+	folder_name = [server],
+	project_name = c.name,
+	package_name = jb.name,
+	executable_name = a.step_name,
+	start_time = CAST(STUFF(STUFF(STUFF(cast(run_date as varchar(8))+RIGHT('00000'+cast(run_time as varchar(6)),6),13,0,':'),11,0,':'),9,0,' ') as datetime),
+	end_time = DATEADD(s,run_duration,CAST(STUFF(STUFF(STUFF(cast(run_date as varchar(8))+RIGHT('00000'+cast(run_time as varchar(6)),6),13,0,':'),11,0,':'),9,0,' ') as datetime)),
+	elapsed_time_min = a.run_duration/60,
+	status = run_status
+  from cte_jobs a
+  left join cte_join b
+  on a.instance_id > isnull(lb_instance_id,0) and a.instance_id <= b.instance_id
+  and a.job_id = b.job_id
+  inner join msdb.dbo.sysjobs jb
+  on a.job_id = jb.job_id
+  inner join msdb.dbo.syscategories c
+  on jb.category_id = c.category_id
+ -- WHERE 
+	--b.instance_id <> a.instance_id 
+  ),
+  cteWE as
 (
 	select 
-		operation_id, event_name, event_count = count(*)
+		operation_id = execution_id, event_name=status, event_count = count(*)
 	from 
-		[catalog].event_messages 
+		cte_executions 
 	where
-		event_name in ('OnError', 'OnWarning')
+		status in (0, 2) and execution_id <> statistics_id 
 	group by
-		operation_id, event_name
+		execution_id, status
 ),
 cteKPI as
 (
 	select
 		operation_id,
-		[errors] = OnError,
-		warnings = OnWarning
+		[errors] = [0],
+		warnings = [2]
 	from
 		cteWE
 	pivot
 		(
-			sum(event_count) for event_name in (OnError, OnWarning)
+			sum(event_count) for event_name in ([0], [2])
 		) p
-),
-cteLoglevel as
-(
-	select
-		execution_id,
-		cast(parameter_value as int) as logging_level
-	from
-		[catalog].[execution_parameter_values]
-	where
-		parameter_name = 'LOGGING_LEVEL'
 )
+
 select top (@executionCount)
 	e.execution_id, 
 	e.project_name,
 	e.package_name,
-	e.project_lsn,
-	environment = isnull(e.environment_folder_name, '') + isnull('\' + e.environment_name,  ''), 
+	project_lsn = 0,
+	environment = '',--isnull(e.environment_folder_name, '') + isnull('\' + e.environment_name,  ''), 
 	e.status, 
 	start_time = format(e.start_time, 'yyyy-MM-dd HH:mm:ss'),
 	end_time = format(e.end_time, 'yyyy-MM-dd HH:mm:ss'),
 	elapsed_time_min = format(datediff(ss, e.start_time, e.end_time) / 60., '#,0.00'),
 	k.warnings,
 	k.errors,
-	l.logging_level
+	logging_level = ''
 from 
-	[catalog].executions e 
+	cte_executions e 
+
 left outer join
 	cteKPI k on e.execution_id = k.operation_id
-left outer join
-	cteLoglevel l on e.execution_id = l.execution_id
+--left outer join
+--	cteLoglevel l on e.execution_id = l.execution_id
 where 
 	e.folder_name like @folderNamePattern
 and
 	e.project_name like @projectNamePattern
 and
-	e.created_time >= dateadd(hour, -@hourspan, @asOfDate)
+	e.start_time >= dateadd(hour, -@hourspan, @asOfDate)
 and
 	(e.[status] = @statusFilter or @statusFilter = 0)
+and
+e.execution_id = statistics_id
 order by 
 	e.execution_id desc
 option
-	(recompile)
-;
+	(recompile);
