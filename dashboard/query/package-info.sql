acbaf5f7e380fd1563558e8ecfa2391690e916e4
Tomasz <t.cieplinski@gmail.com>
2019-02-18 00:17:09 +0100

    Update package-info.sql

--- dashboard/query/package-info.sql
+++ dashboard/query/package-info.sql
@@ -1,11 +1,17 @@
 DECLARE @executionId BIGINT = ?;
 
-SELECT 
-	folder_name, 
-	project_name, 
-	package_name, 
-	project_lsn 
-FROM 
-	[catalog].executions 
-WHERE	
-	execution_id = @executionId;
+	select
+		folder_name = [server],
+		project_name = c.name,
+		package_name = jb.name,
+		project_lsn = 0
+	from
+		msdb.dbo.sysjobhistory jh
+		inner join msdb.dbo.sysjobs jb
+		on jh.job_id = jb.job_id
+		inner join msdb.dbo.syscategories c
+		on jb.category_id = c.category_id
+	where
+		jh.step_id = 0
+	and
+		instance_id = @executionId
