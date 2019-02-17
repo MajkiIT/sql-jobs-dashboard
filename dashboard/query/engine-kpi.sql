9129bc10bfb3d091064404aba35b8d0cf2560a50
Tomasz <t.cieplinski@gmail.com>
2019-02-17 22:36:25 +0100

    Update engine-kpi.sql
    folder => server
    project => category
    status => run_status

--- dashboard/query/engine-kpi.sql
+++ dashboard/query/engine-kpi.sql
@@ -5,18 +5,24 @@ DECLARE @projectNamePattern NVARCHAR(100) = ?;
 
 SET @asOfDate = ISNULL(@asOfDate, SYSDATETIME());
 
-SELECT
-	status_code = ISNULL(e.[status], 0),
-	status_count = COUNT(*)
-FROM
-	[catalog].executions e
-WHERE
-	e.folder_name LIKE @folderNamePattern
-AND
-	e.project_name LIKE @projectNamePattern
-AND
-	e.start_time >= DATEADD(HOUR, -@hourspan, @asOfDate)
-GROUP BY 
-	e.[status]
-WITH
-	ROLLUP
+	select
+		[status_code] = run_status,
+		status_count = COUNT(*)		
+	from
+		msdb.dbo.sysjobhistory jh
+		inner join msdb.dbo.sysjobs jb
+		on jh.job_id = jb.job_id
+		inner join msdb.dbo.syscategories c
+		on jb.category_id = c.category_id
+	where
+		jh.[server] like @folderNamePattern
+	and
+		c.name like @projectNamePattern
+	and
+		jh.step_id = 0
+	AND
+	CAST(STUFF(STUFF(STUFF(cast(run_date as varchar(8))+RIGHT('00000'+cast(run_time as varchar(6)),6),13,0,':'),11,0,':'),9,0,' ') as datetime) >= DATEADD(HOUR, -@hourspan, @asOfDate)
+	GROUP BY 
+		run_status
+	WITH
+		ROLLUP
