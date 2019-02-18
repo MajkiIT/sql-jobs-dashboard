9a7f5a417429c985853152359419db30d1b7d9fd
Tomasz <t.cieplinski@gmail.com>
2019-02-18 23:31:07 +0100

    Update engine-projects.sql

--- dashboard/query/engine-projects.sql
+++ dashboard/query/engine-projects.sql
@@ -1,7 +1,16 @@
-  SELECT DISTINCT 
-	[Server] = originating_server, 
-	[category]
-  FROM [monitorDB].[jobsapp].[V_JobsInfo]
-  ORDER BY
-	[Server],
-	[category]
+;with folders
+as
+(
+	SELECT TOP 1 
+	folder_id = 1, 
+	[name] = f.Server, 
+	[description] = 'Server'
+FROM 
+	msdb.dbo.sysjobhistory f
+)
+select f.*, 
+c.category_id,
+c.name,
+description = 'Job Category' 
+from folders f
+cross join msdb.dbo.syscategories c
