50393819ead4bb82d9664b7bd2ff62bd84f06d5f
Tomasz <t.cieplinski@gmail.com>
2019-02-18 23:38:42 +0100

    Update package-executables.sql

--- dashboard/query/package-executables.sql
+++ dashboard/query/package-executables.sql
@@ -11,9 +11,8 @@ declare @executionIdFilter bigint = ?;
   (
 	select
 	instance_id,
-	run_date,
-	run_time,
-	run_duration,
+	start_time = CAST(STUFF(STUFF(STUFF(cast(run_date as varchar(8))+RIGHT('00000'+cast(run_time as varchar(6)),6),13,0,':'),11,0,':'),9,0,' ') as datetime),
+	run_duration = ((run_duration/1000000)*86400) + (((run_duration-((run_duration/1000000)*1000000))/10000)*3600) + (((run_duration-((run_duration/10000)*10000))/100)*60) + (run_duration-(run_duration/100)*100),
 	run_status,
 	[server],
 	step_name,
@@ -38,8 +37,8 @@ from msdb.dbo.sysjobhistory
 	package_path = 'Job\'+a.step_name, 
 	execution_path = a.server+'\'+jb.name +'\'+a.step_name,
 	executable_name = a.step_name,
-	start_time = format(CAST(STUFF(STUFF(STUFF(cast(run_date as varchar(8))+RIGHT('00000'+cast(run_time as varchar(6)),6),13,0,':'),11,0,':'),9,0,' ') as datetime), 'yyyy-MM-dd HH:mm:ss'),
-	end_time = format(DATEADD(s,run_duration,CAST(STUFF(STUFF(STUFF(cast(run_date as varchar(8))+RIGHT('00000'+cast(run_time as varchar(6)),6),13,0,':'),11,0,':'),9,0,' ') as datetime)), 'yyyy-MM-dd HH:mm:ss'),
+	start_time = format(start_time,'yyyy-MM-dd HH:mm:ss'),
+	end_time = format(DATEADD(s,run_duration,start_time), 'yyyy-MM-dd HH:mm:ss'),
 	execution_duration_min = a.run_duration/60,
 	execution_duration_sec = a.run_duration
   from cte_jobs a
