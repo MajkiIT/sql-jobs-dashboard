141736ebdc0d5c085db6e4ff0ad8e7e20c8ce99a
Tomasz <t.cieplinski@gmail.com>
2019-02-18 22:36:46 +0100

    Update package-details-execution-values.sql

--- dashboard/query/package-details-execution-values.sql
+++ dashboard/query/package-details-execution-values.sql
@@ -1,22 +1,14 @@
 declare @executionId bigint = ?;
 
+declare @executionId bigint = ?;
+
 select 
-	object_type,
-	object_type_desc = 
-		case object_type 
-			when 20 then 'Project'
-			when 30 then 'Package'
-			when 50 then 'System'
-			else 'Unknown'
-		end,
-	parameter_data_type,
-	parameter_name,
-	parameter_value = cast(parameter_value as nvarchar(max)),
-	sensitive,
-	[required], 
-	value_set,
-	runtime_override
-from 
-	[catalog].[execution_parameter_values] 
-where 
-	[execution_id] = @executionId
+	0 object_type,
+	object_type_desc = 'Job',
+	parameter_data_type = 'String',
+	parameter_name = '',
+	parameter_value = 'SQLAGENT',
+	sensitive = 0,
+	[required] = 0, 
+	value_set = 0,
+	runtime_override=0
