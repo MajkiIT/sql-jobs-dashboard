0fba1b76d7621f9585ae3ded53440c95c942dde6
Tomasz <t.cieplinski@gmail.com>
2019-02-18 23:20:05 +0100

    Update engine-folders.sql

--- dashboard/query/engine-folders.sql
+++ dashboard/query/engine-folders.sql
@@ -1,5 +1,6 @@
-  SELECT DISTINCT 
-	[Server] = originating_server
-  FROM [monitorDB].[jobsapp].[V_JobsInfo]
-  ORDER BY
-	[Server]
+SELECT TOP 1 
+	folder_id = 1, 
+	[name] = f.Server, 
+	[description] = ''
+FROM 
+	msdb.dbo.sysjobhistory f
