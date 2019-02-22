5d2af8ac039a9da6ce945cee2c5fc023ced4069e
Tomasz <t.cieplinski@gmail.com>
2019-02-22 15:33:15 +0100

    Update executions-statistics.sql

--- dashboard/query/executions-statistics.sql
+++ dashboard/query/executions-statistics.sql
@@ -29,9 +29,9 @@ with numbers as
 		[execution_id] = instance_id
 	from
 		msdb.dbo.sysjobhistory jh (nolock)
-		inner join msdb.dbo.sysjobs jb
+		inner join msdb.dbo.sysjobs jb (nolock)
 		on jh.job_id = jb.job_id
-		inner join msdb.dbo.syscategories c
+		inner join msdb.dbo.syscategories c (nolock)
 		on jb.category_id = c.category_id
 	where
 		jh.[server] like @folderNamePattern
